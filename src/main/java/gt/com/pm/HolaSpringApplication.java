6b916b25703f24c14e1d5dd29345fd2c82655fc9
Inmer <inmerbalcarcel@gmail.com>
2021-09-01 17:08:20 -0600

    prueba pull

--- src/main/java/gt/com/pm/HolaSpringApplication.java
+++ src/main/java/gt/com/pm/HolaSpringApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HolaSpringApplication {
-
+	//Prueba
 	public static void main(String[] args) {
 		SpringApplication.run(HolaSpringApplication.class, args);
 	}
