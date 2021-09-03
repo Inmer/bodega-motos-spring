35e09d4f73dee22554edab148222fc7133ddd76b
kevinpaz2 <89670517+kevinpaz2@users.noreply.github.com>
2021-09-03 01:50:23 -0600

    Update Motocicleta.java
    Correccion error al editar un cliente no guardaba en base de datos y la pagina devolvia error

--- src/main/java/gt/com/pm/domain/Motocicleta.java
+++ src/main/java/gt/com/pm/domain/Motocicleta.java
@@ -3,6 +3,7 @@ package gt.com.pm.domain;
 import java.io.Serializable;
 import java.util.List;
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import lombok.Data;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
@@ -17,21 +18,21 @@ public class Motocicleta implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id_motocicleta;
-    @NotEmpty
-    private String id_cliente;
+    @NotNull
+    private Long id_cliente;
     @NotEmpty
     private String fecha_recepcion;
     private String fecha_retiro;
-    @NotEmpty
-    private String id_agencia;
+    @NotNull
+    private Long id_agencia;
     @NotEmpty
     private String placa;
     @NotEmpty
     private String numero_motor;
     @NotEmpty
     private String numero_chasis;
-    @NotEmpty
-    private String id_linea;
+    @NotNull
+    private Long id_linea;
     @NotEmpty
     private String modelo;
     private String comentario;
